7fb0e92060adc558163cb3858dadb9304bbea2b9
sibo cheng <sibocheng007@gmail.com>
2025-11-05 10:32:09 +0000

    feat: update Fees component to conditionally disable form inputs and buttons based on selected billing template

--- src/views/operations/Fees.vue
+++ src/views/operations/Fees.vue
@@ -7,25 +7,25 @@
                         <el-button icon="Search"/>
                     </template>
                 </el-input>
-                <el-tree ref="treeRef" :data="data" :props="defaultProps" class="mt" style="max-width: 600px;" :filter-node-method="filterNode"/>
+                <el-tree ref="treeRef" :data="data" :props="defaultProps" class="mt" style="max-width: 600px;" :filter-node-method="filterNode" @node-click="handleNodeClick" />
             </el-card>
         </el-col>
         <el-col :span="18">
             <el-card>
                 <template #header>
                     <div class="card-header">
-                        <h3>{{title}}:计费模版</h3>
-                    </div>
+                        <h3>{{title ? title + ':' : ''}}计费模版</h3>
+                    </div>  
                 </template>
                 <el-form ref="formRef" :model="form" :rules="rules" label-width="auto">
                     <el-form-item label="模版名称" prop="name">
-                        <el-input v-model="form.name" placeholder="请输入模版名称" style="width: 40%;"/>
+                        <el-input v-model="form.name" placeholder="请输入模版名称" :disabled="!title" style="width: 40%;"/>
                     </el-form-item>
                     <el-form-item :label="'收费时段' + (index + 1)" v-for="(item, index) in form.date" :key="index">
                         <el-row :gutter="20" style="width: 60%;">
                             <el-col :span="8">
                                 <el-form-item label="开始时间" :prop="`date.${index}.startTime`" :rules="[{ required: true, message: '请选择开始时间', trigger: 'blur' }]">
-                                    <el-time-picker v-model="item.startTime" placeholder="请选择开始时间" style="width: 100%;" value-format="HH:mm:ss"></el-time-picker>
+                                    <el-time-picker v-model="item.startTime" placeholder="请选择开始时间" style="width: 100%;" value-format="HH:mm:ss" :disabled="!title"></el-time-picker>
                                 </el-form-item>
                             </el-col>
                             <el-col :span="1">
@@ -33,29 +33,29 @@
                             </el-col>
                             <el-col :span="8">
                                 <el-form-item label="结束时间" :prop="`date.${index}.endTime`" :rules="[{ required: true, message: '请选择结束时间', trigger: 'blur' }]">
-                                    <el-time-picker v-model="item.endTime" placeholder="请选择结束时间" style="width: 100%;" value-format="HH:mm:ss"></el-time-picker>
+                                    <el-time-picker v-model="item.endTime" placeholder="请选择结束时间" style="width: 100%;" value-format="HH:mm:ss" :disabled="!title"></el-time-picker>
                                 </el-form-item>     
                             </el-col>
                             <el-col :span="6">
                                 <el-form-item label="单价" :prop="`date.${index}.unitPrice`" :rules="[{ required: true, message: '请输入单价', trigger: 'blur' }]">
-                                    <el-input v-model="item.unitPrice" placeholder="请输入单价" style="width: 100%;"/>
+                                    <el-input v-model="item.unitPrice" placeholder="请输入单价" style="width: 100%;" :disabled="!title"/>
                                 </el-form-item>
                             </el-col>
                         </el-row>
                     </el-form-item>
-                    <el-button class="mb" type="primary" @click="addTime">新增收费时段</el-button>
+                    <el-button class="mb" type="primary" @click="addTime" :disabled="!title">新增收费时段</el-button>
                     <el-form-item label="服务费" prop="serviceFee">
-                        <el-input v-model="form.serviceFee" placeholder="请输入服务费" style="width: 40%;"/>
+                        <el-input v-model="form.serviceFee" placeholder="请输入服务费" style="width: 40%;" :disabled="!title"/>
                     </el-form-item>
                     <el-form-item label="停车费" prop="parkingFee">
-                        <el-input v-model="form.parkingFee" placeholder="请输入停车费" style="width: 40%;"/>
+                        <el-input v-model="form.parkingFee" placeholder="请输入停车费" style="width: 40%;" :disabled="!title"/>
                     </el-form-item>
                     <el-form-item label="备注" prop="remarks">
-                        <el-input v-model="form.remarks" type="textarea" :rows="3" placeholder="请输入备注"/>
+                        <el-input v-model="form.remarks" type="textarea" :rows="3" placeholder="请输入备注" :disabled="!title"/>
                     </el-form-item>
                     <el-form-item>
-                        <el-button type="primary" @click="handleSubmit">提交</el-button>
-                        <el-button>清空</el-button>
+                        <el-button type="primary" @click="handleSubmit" :disabled="!title">提交</el-button>
+                        <el-button @click="resetForm" :disabled="!title">清空</el-button>
                     </el-form-item>
                 </el-form>
             </el-card>
@@ -133,6 +133,25 @@ const handleSubmit = () => {
             }
         }
     })
+}
+
+const handleNodeClick = (data) =>{
+    if(!data.children){
+        title.value = data.label;
+        resetForm();
+    }
+}
+
+const resetForm = () => {
+    form.value = {
+        name: '',
+        serviceFee: 0,
+        parkingFee: 0,
+        remarks: '',
+        date: [
+            {startTime: '', endTime: '', unitPrice: null},
+        ],
+    }
 }
 
 </script>
