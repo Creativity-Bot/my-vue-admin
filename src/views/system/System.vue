782e14dde28e91f86842eb8245733da7e76c8901
sibo cheng <sibocheng007@gmail.com>
2025-11-12 13:40:24 +0000

    refactor: remove deprecated System API and update System component to use new data loading method

--- src/views/system/System.vue
+++ src/views/system/System.vue
@@ -17,13 +17,13 @@
                 </el-select>
             </el-col>
             <el-col :span="6">
-                <el-button type="primary" @click="getPermissionListData">查询</el-button>
+                <el-button type="primary" @click="loadData">查询</el-button>
                 <el-button @click="handleReset">重置</el-button>
             </el-col>
         </el-row>
     </el-card>
     <el-card class="mt">
-        <el-table :data="permissionList" v-loading="loading">
+        <el-table :data="listData" v-loading="isLoading">
             <el-table-column type="index" label="序号" width="80" />
             <el-table-column prop="account" label="账号"></el-table-column>
             <el-table-column prop="name" label="姓名"></el-table-column>
@@ -73,44 +73,19 @@
     </el-card>
 </template>
 <script setup>
-import { getPermissionList } from '@/api/system';
-import { onMounted, ref } from 'vue';
-import {usePagination} from '@/hooks/usePagination';
-import { ElMessage } from 'element-plus';
-const permissionList = ref([]);
-const loading = ref(false);
+import { ref } from 'vue';
+import { useHttp } from '@/hooks/useHttp';
 const searchParams = ref({
     name: '',
     department: '0',
 });
-const getPermissionListData = async () => {
-    try {
-        loading.value = true;
-        const { data: { list, total } } = await getPermissionList({
-            ...searchParams.value,
-            page: pageInfo.currentPage,
-            pageSize: pageInfo.pageSize,
-        });
-
-        permissionList.value = list;
-        totals.value = total;
-    } catch (error) {
-        console.log(error);
-        ElMessage.error('获取权限列表失败');
-    }finally{
-        loading.value = false;
-    }
-}
-const { totals, pageInfo, handleSizeChange, handleCurrentChange, resetPagination } = usePagination(getPermissionListData);
+const { listData, isLoading, totals, pageInfo, loadData, handleSizeChange, handleCurrentChange, resetPagination } = useHttp('/permissionList', searchParams);
 const handleReset = () => {
     searchParams.value = {
         name: '',
         department: '0',
     };
     resetPagination();
-    getPermissionListData();
+    loadData();
 }
-onMounted(() => {
-    getPermissionListData();
-})
 </script>
